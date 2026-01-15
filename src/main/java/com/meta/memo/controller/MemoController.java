fcd22e971e1e23817d6313d83b9cb0a4e841a771
Lim <apple2567338@gmail.com>
2026-01-15 16:21:46 +0900

    feat : Memo를 저장할 "임시 데이터베이스(인메모리) 선언"

--- src/main/java/com/meta/memo/controller/MemoController.java
+++ src/main/java/com/meta/memo/controller/MemoController.java
@@ -1,9 +1,15 @@
 package com.meta.memo.controller;
 
+import com.meta.memo.domain.Memo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/memos")
 public class MemoController {
+    // 임시 데이터베이스(내장 메모리 배열)
+    private final Map<Long, Memo> memoList = new HashMap<>();
 }
