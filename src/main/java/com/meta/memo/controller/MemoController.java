847cd959ca4829f81c330f6a51143ba23f81ed9c
Lim <apple2567338@gmail.com>
2026-01-15 17:40:36 +0900

    feat : Memo Delete 메모 삭제 기능 구현

--- src/main/java/com/meta/memo/controller/MemoController.java
+++ src/main/java/com/meta/memo/controller/MemoController.java
@@ -58,4 +58,17 @@ public class MemoController {
             throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
         }
     }
+
+    @DeleteMapping("{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        // 해당 id의 메모가 데이터베이스에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            // true면, 해당 메모 삭제
+            memoList.remove(id);
+            return id;
+        }
+        else {
+            throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
+        }
+    }
 }
