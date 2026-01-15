d702af3f891fcf1d403fa1700f34d96b4dd1a4b0
Lim <apple2567338@gmail.com>
2026-01-15 16:13:06 +0900

    feat : Memo Controller 기본 코드 생성

---
+++ src/main/java/com/meta/memo/controller/MemoController.java
@@ -0,0 +1,9 @@
+package com.meta.memo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/memos")
+public class MemoController {
+}
