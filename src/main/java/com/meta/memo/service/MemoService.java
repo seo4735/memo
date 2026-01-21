6c4a72f2e8ba6d21f95d02ca8e29d9a5d8b269b7
Lim <apple2567338@gmail.com>
2026-01-21 14:33:32 +0900

    refactor : Service 계층에서 트랜잭션 설정(CUD) / 읽기전용(R)

--- src/main/java/com/meta/memo/service/MemoService.java
+++ src/main/java/com/meta/memo/service/MemoService.java
@@ -6,12 +6,15 @@ import com.meta.memo.dto.MemoResponseDto;
 import com.meta.memo.repository.MemoRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@Component
+@Service
+@org.springframework.transaction.annotation.Transactional
 public class MemoService {
     // 멤버 변수 선언
     private final MemoRepository memoRepository;
@@ -21,6 +24,7 @@ public class MemoService {
         this.memoRepository = memoRepository;
     }
 
+    @Transactional
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto memoRequestDto) {
         // RequestDto -> Entity 변환
         Memo newMemo = new Memo(memoRequestDto);
@@ -30,17 +34,20 @@ public class MemoService {
         return memoResponseDto;
     }
 
+
     public List<MemoResponseDto> getMemos() {
         List<MemoResponseDto> memoResponseDtoList = memoRepository.findAll().stream()
                 .map(MemoResponseDto::new).toList();
         return memoResponseDtoList;
     }
 
+    @Transactional
     public Memo getMemoById(Long id) {
         return memoRepository.findById(id).orElseThrow(() ->
                 new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다."));
     }
 
+    @Transactional
     public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto memoRequestDto) {
         // 해당 id의 메모가 존재하는지 확인
         Memo foundMemo = getMemoById(id);
@@ -49,6 +56,7 @@ public class MemoService {
         return id;
     }
 
+    @Transactional
     public Long deleteMemo(@PathVariable Long id) {
         // 해당 id의 메모가 존재하는지 확인
         Memo foundMemo = getMemoById(id);
